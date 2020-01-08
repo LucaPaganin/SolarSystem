aca6f8a77d647d065079e7aaf257eee5f1c5aacc
Luca Paganin <luca.paganin@ge.infn.it>
2020-01-08 12:28:15 +0100

    Added print out of initial conditions

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  
+//
 //
 //  Created by Luca Paganin on 06/01/2020.
 //
@@ -9,46 +9,47 @@
 #include "Grave.h"
 #include "SistemaSolare.h"
 
-int main(){
-	
+int main(int argc, const char* argv[]){
+
 	std::ofstream output_file("output/temporal_evolution.txt");
-    
-    Vettore3D r(1,2,3);
-    Vettore3D v(1,2,3);
-    double m=1;
 
-    Grave g(r,v,m);
+  Vettore3D r(1,2,3);
+  Vettore3D v(1,2,3);
+  double m=1;
 
-    std::vector<Grave> planets{g};
+  Grave g(r,v,m), g2(Vettore3D(0,0,0), Vettore3D(0,0,0), 2*m);
 
-    for (auto p: planets){
-        std::cout << p.R() << " " << p.V() << " " << p.M() << std::endl;
-    }
+  std::vector<Grave> planets{g,g2};
 
-    /*	
+  for (const auto &p: planets){
+		auto index = &p - &planets[0];
+		std::cout << "Planet " << index+1 << ": " << std::endl;
+    std::cout <<  "Coordinates: (" << p.R() << "); Velocity: (" << p.V() << "); Mass = " << p.M() << std::endl;
+  }
+
+  /*
 	std::ifstream input_file("input_file.h");
-	
-	
+
+
 	SistemaSolare system;
-	
+
 	system.ReadInitialConditions(input_file);
-	
+
 	//system.print_system();
-	
+
 	int N = 1e5;
 	double dt=1e-5;
-	
+
 	for (int i=0; i<N; ++i){
 		if (i%100 == 0)
 			system.print_planets_coords(output_file);
-		
+
 		system.EulerCromerStep(dt);
 	}
-	
-	
+
+
 	input_file.close();
-	output_file.close();
 	*/
-	
+	output_file.close();
 	return 0;
 }
