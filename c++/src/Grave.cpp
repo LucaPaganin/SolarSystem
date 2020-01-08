62908727939c7154bd9ab3eb79628c6bd613952a
Luca Paganin <luca.paganin@ge.infn.it>
2020-01-08 12:25:29 +0100

    Marked getter methods as const

--- c++/src/Grave.cpp
+++ c++/src/Grave.cpp
@@ -1,15 +1,15 @@
 //
 //  Grave.cpp
-//  
+//
 //
 //  Created by Luca Paganin on 06/01/2020.
 //
 
 #include "Grave.h"
 
-Vettore3D Grave::R(){return m_R;}
+Vettore3D Grave::R() const {return m_R;}
 
-Vettore3D Grave::V(){return m_V;}
+Vettore3D Grave::V() const {return m_V;}
 
 void Grave::R(const Vettore3D& r){m_R = r;}
 
@@ -18,10 +18,10 @@ void Grave::V(const Vettore3D& v){m_V = v;}
 double Grave::M() const {return m_M;}
 
 Vettore3D Grave::CampoGravitazionale(const Grave& g){
-	
+
 	Vettore3D rel_pos = m_R - g.m_R;
 	Vettore3D field = rel_pos;
-	
+
 	if (rel_pos == Vettore3D(0,0,0)){
 		//std::cout << "Error, can't evaluate gravitational field of point source into source position." << std::endl;
 	}
@@ -32,13 +32,13 @@ Vettore3D Grave::CampoGravitazionale(const Grave& g){
 }
 
 Vettore3D Grave::CampoGravitazionale(const std::vector<Grave>& sources){
-	
+
 	Vettore3D field(0,0,0);
-	
+
 	for (auto src : sources) {
 		field += this->CampoGravitazionale(src);
 	}
-	
+
 	return field;
-	
+
 }
