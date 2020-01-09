cfaa0bc27a806bc75955b94cd6b6ab6fb4212bd8
Luca Paganin <luca.paganin@ge.infn.it>
2020-01-09 18:33:28 +0100

    Added correction comment

--- python/plotter.py
+++ python/plotter.py
@@ -14,7 +14,14 @@ data = np.loadtxt(datafile_path)
 
 n_planets = int(data.shape[1]/3)
 
+"""
+Here we are picking the wrong coordinates!! We are collecting the following:
+x1 y1 z1, y1 z1 x2, z1 x2 y2... and so on
+Need to put a factor 3 in front of index i of subrange i:i+3
+"""
 planet_coords = [data[:,i:i+3] for i in range(n_planets)]
+
+print(planet_coords[0])
 
 colors = matplotlib.cm.rainbow(np.linspace(0, 1, n_planets))
 
