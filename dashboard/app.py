a135c34df8236b5f69ffaa21698168d36f941169
Luca Paganin <luca.paganin@ge.infn.it>
2022-08-20 22:04:58 +0200

    moved app.py

---
+++ dashboard/app.py
@@ -0,0 +1,39 @@
+import os
+from dash import html, Dash
+import dash_bootstrap_components as dbc
+from dashboard.utils import dashboardlogging as Logger
+from dashboard.callbacks import init_callbacks
+from dashboard.components import standardSolarSystemTab
+
+logger = Logger.GetLogger('app', configure=True)
+
+
+def init_dashboard():
+    """Create a Plotly Dash dashboard."""
+    dash_app = Dash(
+        title="Solar System Simulator",
+        # routes_pathname_prefix='/dashboards/solarsystem/',
+        external_stylesheets=[
+            dbc.themes.BOOTSTRAP
+        ]
+    )
+    logger.info("initializing dashboard")
+
+    # Create Dash Layout
+    dash_app.layout = html.Div(
+        id='dash-container',
+        children=[
+            dbc.Tabs([
+                standardSolarSystemTab()
+            ], active_tab='solar-system')
+        ])
+    logger.info("initializing callbacks")
+    init_callbacks(dash_app)
+
+    return dash_app
+
+app = init_dashboard()
+
+if __name__ == '__main__':
+    debug = os.getenv("DASH_DEBUG") == "true"
+    app.run_server(debug=debug)
--- dashboard/droplet-config.sh
+++ dashboard/droplet-config.sh
@@ -3,8 +3,8 @@
 apt-get upgrade -y
 apt-get update -y
 echo "Installing python, nginx and supervisor"
-apt-get install python3-pip python3-venv python3-dev build-essential \
-                python3-setuptools libssl-dev libffi-dev supervisor nginx
+apt-get install -y python3-pip python3-venv python3-dev build-essential \
+                   python3-setuptools libssl-dev libffi-dev supervisor nginx
 
 python3 -m venv venv
 source ./venv/bin/activate
