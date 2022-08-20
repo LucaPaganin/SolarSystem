7e82ea81d816dcc44293e4d3a65b27d44dd303d5
Luca Paganin <luca.paganin@ge.infn.it>
2022-08-20 22:59:48 +0200

    add supervisor and nginx config

--- dashboard/droplet-config.sh
+++ dashboard/droplet-config.sh
@@ -5,10 +5,8 @@ apt-get upgrade -y
 apt-get update -y
 echo "Installing python, nginx and supervisor"
 apt-get install -y python3-pip python3-venv python3-dev build-essential \
-                   python3-setuptools python3-poetry libssl-dev libffi-dev \
-                   supervisor nginx
-python3 -m venv venv
-source ./venv/bin/activate
+                   python3-setuptools python3-poetry python3-cachecontrol \ 
+                   libssl-dev libffi-dev supervisor nginx net-tools
 pip install poetry
 poetry install
 
@@ -24,3 +22,14 @@ echo "Returning to ${CWD}"
 cd $(PWD)
 
 cat supervisor.txt > /etc/supervisor/conf.d/dash.conf
+
+supervisorctl reload
+
+DROPLET_IP=$(ifconfig | egrep -o "inet [0-9\.]+" | head -1 | cut -d ' ' -f 2)
+sed -i "s/server_name .*$/server_name ${DROPLET_IP};/" nginx.txt
+
+rm -f /etc/nginx/sites-enabled/default
+
+cat nginx.txt > /etc/nginx/sites-enabled/dash
+
+service nginx reload
