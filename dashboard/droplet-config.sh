5d8874a6796abc648ac8615810941953980db2fc
Luca Paganin <luca.paganin@ge.infn.it>
2022-08-20 22:10:56 +0200

    moved app

--- dashboard/droplet-config.sh
+++ dashboard/droplet-config.sh
@@ -4,8 +4,8 @@ apt-get upgrade -y
 apt-get update -y
 echo "Installing python, nginx and supervisor"
 apt-get install -y python3-pip python3-venv python3-dev build-essential \
-                   python3-setuptools libssl-dev libffi-dev supervisor nginx
-
+                   python3-setuptools python3-poetry libssl-dev libffi-dev \
+                   supervisor nginx
 python3 -m venv venv
 source ./venv/bin/activate
 pip install poetry
