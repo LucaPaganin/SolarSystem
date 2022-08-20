9d4e46587d16229437ef6289e801e0577305bf4b
Luca Paganin <luca.paganin@ge.infn.it>
2022-08-20 21:27:04 +0200

    add config script

--- dashboard/droplet-config.sh
+++ dashboard/droplet-config.sh
@@ -6,8 +6,6 @@ echo "Installing python, nginx and supervisor"
 apt-get install python3-pip python3-venv python3-dev build-essential \
                 python3-setuptools libssl-dev libffi-dev supervisor nginx
 
-mkdir -p /root/workdir
-cd /root/workdir
 python3 -m venv venv
 source ./venv/bin/activate
 pip install poetry
